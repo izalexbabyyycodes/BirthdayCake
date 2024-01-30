937a43ecd9444b9716f2af4e8d9ae9258ad0d083
amnuxoll <amnuxoll@hotmail.com>
2024-01-30 10:30:01 -0800

    Nuxoll attempts to update for Spring 2024 semester
    The version of gradle/java  used for this was hopelessly out of date.

--- app/src/main/java/cs301/birthdaycake/MainActivity.java
+++ app/src/main/java/cs301/birthdaycake/MainActivity.java
@@ -1,8 +1,9 @@
 package cs301.birthdaycake;
 
 import android.content.pm.ActivityInfo;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
